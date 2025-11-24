d4dfa7747d76ebf12c7f25adc92bbf4e6181f5b7
matei8787 <matei8787@gmail.com>
2025-11-24 17:20:41 +0200

    Gata_acum1

--- vpn_provisioner/entrypoint.sh
+++ vpn_provisioner/entrypoint.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 set -e
 
-python manage.py collectstatic --no-inpuit
+python manage.py collectstatic --no-input
 python manage.py makemigrations --no-input
 python manage.py migrate --no-input
 
