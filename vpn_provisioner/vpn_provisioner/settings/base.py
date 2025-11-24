80d8d41e1d1eb6ccfc1e8a10705dee75bedf523d
matei8787 <matei8787@gmail.com>
2025-11-24 16:21:34 +0200

    Gata_acum

--- vpn_provisioner/vpn_provisioner/settings/base.py
+++ vpn_provisioner/vpn_provisioner/settings/base.py
@@ -28,7 +28,7 @@ VERIFY_SSL = os.environ.get("VERIFY_SSL") == "True"
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get("DEBUG") == "True"
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = os.environ.get("ALLOWED_HOSTS").split(",")
 
 
 # Application definition
