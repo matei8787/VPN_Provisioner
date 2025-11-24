e4c2eb534ea12ed35f7d0fa82b0312bf46f1e72b
matei8787 <matei8787@gmail.com>
2025-11-24 18:30:01 +0200

    added nginx bun

--- vpn_provisioner/entrypoint.sh
+++ vpn_provisioner/entrypoint.sh
@@ -3,6 +3,8 @@ set -e
 
 python manage.py collectstatic --no-input
 python manage.py makemigrations --no-input
+python manage.py makemigrations core --no-input
 python manage.py migrate --no-input
+python manage.py migrate core --no-input
 
 exec "$@"
