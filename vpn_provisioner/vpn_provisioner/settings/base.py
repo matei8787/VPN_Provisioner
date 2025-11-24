7005a78b08c2b59f0322ea0c3143f88c610342a3
matei8787 <matei8787@gmail.com>
2025-11-24 18:09:05 +0200

    added nginx bun

--- vpn_provisioner/vpn_provisioner/settings/base.py
+++ vpn_provisioner/vpn_provisioner/settings/base.py
@@ -120,13 +120,13 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
 STATIC_ROOT = '/usr/share/nginx/html/ovpn/staticfiles/'
 
 # Media files
 
 MEDIA_ROOT = '/usr/share/nginx/html/ovpn/mediafiles/'
-MEDIA_URL = 'media/'
+MEDIA_URL = '/media/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
