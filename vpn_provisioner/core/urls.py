d7db8e901f00e9dab387842cb0095624ce976d93
matei8787 <matei8787@gmail.com>
2025-11-24 18:51:17 +0200

    added nginx bun

--- vpn_provisioner/core/urls.py
+++ vpn_provisioner/core/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 from .views import index_view, vpn_export_view
 urlpatterns = [
-    path('index/', index_view, name="index"),
+    path('', index_view, name="index"),
     path('download/', vpn_export_view, name="export")
 ]
